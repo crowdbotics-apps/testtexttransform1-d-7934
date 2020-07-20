d37bf0cd21cec6398f3d35af72522263dd8d9f27
Crowdbotics <team@crowdbotics.com>
2020-07-20 16:55:34 +0000

    Update components

--- src/features/BlankScreen29146/screens/blank.js
+++ src/features/BlankScreen29146/screens/blank.js
@@ -22,7 +22,7 @@ export class _Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = { Input_3: "" }
 
   render = () => (
     <View
@@ -129,6 +129,44 @@ export class _Blank extends React.Component {
       >
         Sample text content
       </Text>
+      <Input
+        placeholder="Number Input Placeholder"
+        editable={true}
+        keyboardType="numeric"
+        textStyle={{ fontSize: 20, color: "#000000" }}
+        style={{
+          width: "100%",
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: 20,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textShadow: "0pt 0pt 0pt #000000",
+          textDecoration: "none",
+          textTransform: "none"
+        }}
+        value={this.state.Input_3}
+        onChangeText={nextValue => this.setState({ Input_3: nextValue })}
+      />
     </View>
   )
 }
